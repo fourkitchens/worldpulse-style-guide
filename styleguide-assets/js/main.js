bb568d394b9041f510387be0a93673c04fd9e9dc
Taylor Smith <tsmith@tsmithcreative.com>
2014-03-24 02:36:24 +0000

    Make component pages and nav a bit better

--- styleguide-assets/js/main.js
+++ styleguide-assets/js/main.js
@@ -1,9 +1,9 @@
 (function($) {
   $(document).ready(function(){
     if ( $('.styleguide-nav').length && $('.styleguide-nav').hasClass('collapse') ) {
-      $('.styleguide-nav li.heading').nextUntil('li.heading').hide();
+      $('.styleguide-nav li.heading').nextUntil('li.heading').not('.current').hide();
       $('.styleguide-nav li.heading').click(function(){
-        $('.styleguide-nav li.heading').nextUntil('li.heading').hide();
+        $('.styleguide-nav li.heading').nextUntil('li.heading').not('.current').hide();
         $(this).nextUntil('li.heading').show();
       });
     }
